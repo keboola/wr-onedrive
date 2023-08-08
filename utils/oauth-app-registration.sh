9c70c7840b5763b6be8de55bfb43a2aa990b989b
Michal Jurečko <michal.jurecko@gmail.com>
2023-08-08 22:19:17 +0200

    Update OAuth registration script

--- utils/oauth-app-registration.sh
+++ utils/oauth-app-registration.sh
@@ -6,10 +6,9 @@ set -o nounset          # Disallow expansion of unset variables
 set -o pipefail         # Use last non-zero exit code in a pipeline
 #set -o xtrace          # Trace the execution of the script (debug)
 
-# Load env variables
+# Load env variables from .env file, but not overwrite the existing one
 if [ -f ".env" ]; then
-  IFS=' ' read -ra envPairs <<< "$(xargs < .env)" >/dev/null 2>&1
-  if [ -n "${envPairs:-}" ]; then export "${envPairs[@]}"; fi
+  source <(grep -v '^#' .env | sed -E 's|^([^=]+)=(.*)$|: ${\1=\2}; export \1|g')
 fi
 
 # Required environment variables
@@ -21,7 +20,17 @@ SCRIPT_DIR=$(dirname "$SCRIPT")
 SCRIPT_FILENAME=$(basename "$SCRIPT")
 AZ_CLI_IMG="mcr.microsoft.com/azure-cli"
 
-# If NOT runned in Docker container AND "az" executable not exists locally ...
+# Permissions
+# https://www.shawntabrizi.com/aad/common-microsoft-resources-azure-active-directory/
+api_id="00000003-0000-0000-c000-000000000000"
+# https://github.com/stephaneey/azure-ad-vsts-extension/blob/master/overview.md
+declare -A permissions
+permissions["offline_access"]="7427e0e9-2fba-42fe-b0c0-848c9e6a8182"
+permissions["User.Read"]="e1fe6dd8-ba31-4d61-89e7-88639da4683d"
+permissions["Files.ReadWrite.All"]="863451e7-0667-486c-a5d6-d135439485f0"
+permissions["Sites.ReadWrite.All"]="89fe6a52-be36-487e-b7d8-d061c450a026"
+
+# If NOT run in the Docker container AND "az" executable not exists locally ...
 if [ ! -f /.dockerenv ] && ! command -v az >/dev/null 2>&1; then
   # ... run script in Docker container
   echo "Running in Docker container ..."
@@ -43,39 +52,36 @@ else
 fi
 
 # Get app id if exists
-echo "Testing if the application exists"
+echo "Testing if the application \"$OAUTH_APP_NAME\" exists ..."
 OAUTH_APP_ID=$(az ad app list --output tsv --filter "displayName eq '$OAUTH_APP_NAME'" --query "[].appId | [0]")
 
 # Create app if not exists
 if [ -z "$OAUTH_APP_ID" ]; then
   echo "Application does not exist."
   echo "Creating application \"$OAUTH_APP_NAME\""
-  OAUTH_APP_SECRET=`openssl rand -base64 32`
   OAUTH_APP_ID=$(
-    az ad app create \
-      --output tsv \
-      --display-name "$OAUTH_APP_NAME" \
-      --oauth2-allow-implicit-flow true \
-      --available-to-other-tenants true \
-      --end-date '2050-12-31' \
-      --password "$OAUTH_APP_SECRET" \
-      --query "appId"
+   az ad app create \
+        --output tsv \
+        --query "appId" \
+        --is-fallback-public-client false \
+        --display-name "$OAUTH_APP_NAME" \
+        --enable-access-token-issuance true \
+        --sign-in-audience AzureADandPersonalMicrosoftAccount \
+        --end-date '2050-12-31'
   )
   echo "Application created, OAUTH_APP_ID=\"$OAUTH_APP_ID\""
+
+  # Get secret
+  OAUTH_APP_SECRET=$(az ad app credential reset \
+    --output tsv \
+    --query "password" \
+    --id "$OAUTH_APP_ID"
+  )
+
   echo "SAVE SECRET KEY!!! -> OAUTH_APP_SECRET=\"$OAUTH_APP_SECRET\""
 else
   echo "Application already exists, OAUTH_APP_ID=\"$OAUTH_APP_ID\""
 fi
-
-# Permissions
-# https://www.shawntabrizi.com/aad/common-microsoft-resources-azure-active-directory/
-graph_api_id="00000003-0000-0000-c000-000000000000"
-# https://github.com/stephaneey/azure-ad-vsts-extension/blob/master/overview.md
-declare -A permissions
-permissions["offline_access"]="7427e0e9-2fba-42fe-b0c0-848c9e6a8182"
-permissions["User.Read"]="e1fe6dd8-ba31-4d61-89e7-88639da4683d"
-permissions["Files.ReadWrite.All"]="863451e7-0667-486c-a5d6-d135439485f0"
-permissions["Sites.ReadWrite.All"]="89fe6a52-be36-487e-b7d8-d061c450a026"
 
 # Load active permissions
 echo "Checking permission"
@@ -91,36 +97,21 @@ for perm_name in "${!permissions[@]}"; do
   fi
 done
 
+echo "Active permissions: $activePerms"
+
 if [ ${#perms_arg[@]} -ne 0 ]; then
   echo "Setting permission"
-  if ! az ad app permission add --id "$OAUTH_APP_ID" --api "$graph_api_id" --api-permissions "${perms_arg[@]}" 2>/dev/null; then
+  if ! az ad app permission add --id "$OAUTH_APP_ID" --api "$api_id" --api-permissions "${perms_arg[@]}" 2>/dev/null; then
     echo "WARNING: Error setting permissions."
     echo "WARNING: Please edit it manually in Azure Portal -> App registrations -> $OAUTH_APP_NAME -> Permissions"
   fi
 fi
 
-# Set public client = false
-echo "Checking \"publicClient\" property"
-publicClient=$(az ad app list --output tsv --filter "displayName eq '$OAUTH_APP_NAME'"  --query "[].publicClient | [0]")
-if [ "$publicClient" != "false" ]; then
-  echo "Setting publicClient=false"
-  az ad app update --id "$OAUTH_APP_ID"  --set "publicClient=false" || true
-fi
-
-# Allow login with all types of account
-# This currenty cannot by set through API
-echo "Checking \"signInAudience\" property"
-signInAudience=$(az ad app list --output tsv --filter "displayName eq '$OAUTH_APP_NAME'"  --query "[].signInAudience | [0]")
-if [ "$signInAudience" != "AzureADandPersonalMicrosoftAccount" ]; then
-  echo "WARNING: Property \"signInAudience\" = \"$signInAudience\", but it should by set to \"AzureADandPersonalMicrosoftAccount\"."
-  echo "WARNING: User won't be able to sign in with all types of accounts."
-  echo "WARNING: Please edit it manually in Azure Portal -> App registrations -> $OAUTH_APP_NAME -> Manifest"
-fi
 
 # Print ENV variables
 echo -e "\nDone\n"
 echo -e "\n-----------------------------------------------------"
 echo -e "Please, add these envrioment variables to \".env\" file:\n"
-echo "OAUTH_APP_NAME=$OAUTH_APP_NAME"
+echo "OAUTH_APP_NAME=\"$OAUTH_APP_NAME\""
 echo "OAUTH_APP_ID=$OAUTH_APP_ID"
 echo "OAUTH_APP_SECRET=${OAUTH_APP_SECRET:-...}"
